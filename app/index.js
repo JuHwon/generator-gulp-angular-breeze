2e2ededd1f10cab16ce3bef74d615f1dca47ce87
JuHwon <juer.wagner@gmail.com>
2015-08-06 22:40:30 +0200

    impl app name prompt

--- app/index.js
+++ app/index.js
@@ -1,11 +1,52 @@
 var generators = require('yeoman-generator');
+var path = require('path');
+var yosay = require('yosay');
 
-module.exports = generators.NamedBase.extend({
+module.exports = generators.Base.extend({
   constructor: function () {
-    generators.NamedBase.apply(this, arguments);
+    generators.Base.apply(this, arguments);
+
+    this.argument('appName', { type: String, required: false });
+    this.appName = this._.camelize(this._.slugify(this._.humanize(this.appName)));
   },
 
-  method1: function() {
-    console.log('method1 just ran');
+  welcome: function() {
+    this.log(yosay(
+      'welcome to the gulp-angular-breeze generator!'
+    ));
+  },
+
+  prompting: function() {
+    if (this.appName) {
+      return;
+    }
+
+    var done = this.async();
+
+    var prompts = [{
+      type: 'input',
+      name: 'appName',
+      message: 'What would you like to name the app?',
+      default: this.appName || path.basename(process.cwd())
+    }];
+
+    this.prompt(prompts, function(answers){
+      this.appName = answers.appName;
+      this.appName = this.appName || path.basename(process.cwd());
+      done();
+    }.bind(this));
+  },
+
+  displayName: function() {
+    this.log('Creating ' + this.appName
+      + ' app based on gulp-angular-breeze generator.');
+  },
+
+  scaffoldFolders: function() {
+    this.mkdir('src');
+    this.mkdir('src/client');
+    this.mkdir('src/client/app');
+    this.mkdir('src/server');
   }
+
 });
